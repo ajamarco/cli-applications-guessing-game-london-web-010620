780a94032cdcaeb1af6ec8760216e30263c8199d
Alexandre Jamarco <alexandre.j@gmail.com>
2019-12-25 14:59:43 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,11 +2,7 @@ def run_guessing_game
   number = rand(1..6)
   puts "try to guess the number!"
   guessed_number = gets.chomp.strip
-  if guessed_number.downcase == "exit"
-    puts "Goodbye!"
-  elsif number.to_s == guessed_number
-    puts "You guessed the correct number!"
-  else
-    puts "Sorry! The computer guessed #{number}."
+  if guessed_number == number.to_s 
+    
   end
 end
