c3553c2b05e82c8e8331f25ccf1a1c03c429270d
Alexandre Jamarco <alexandre.j@gmail.com>
2019-12-25 15:00:22 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,6 +3,8 @@ def run_guessing_game
   puts "try to guess the number!"
   guessed_number = gets.chomp.strip
   if guessed_number == number.to_s 
-    
+    puts "You guessed the correct number!"
+  elsif guessed_number.downcase == "exit"
+    puts "Goodbye!"
   end
 end
