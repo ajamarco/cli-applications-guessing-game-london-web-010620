8d2cd925ceee8c5f03d419bddaf3758aaee27f1b
Alexandre Jamarco <alexandre.j@gmail.com>
2019-12-25 14:33:48 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -8,6 +8,6 @@ def run_guessing_game
   elsif guessed_number.to_i == number
     puts "You guessed the correct number!"
   else
-    puts "Sorry! The computer guessed #{number}"
+    puts "Sorry! The computer guessed #{number}."
   end
 end
