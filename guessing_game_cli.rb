814e62a5a9d51a28dffc5e30bf1957f841fd836e
Alexandre Jamarco <alexandre.j@gmail.com>
2019-12-25 14:46:07 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -5,7 +5,6 @@ def run_guessing_game
   number = rand(1..6)
   puts "try to guess the number!"
   guessed_number = gets.chomp.strip
-  binding.pry
   if guessed_number == "exit"
     puts "Goodbye!"
   else
