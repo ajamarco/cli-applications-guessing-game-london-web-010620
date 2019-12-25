2d31e9e6f411934e63b856adcbe97ef7c87b0ee9
Alexandre Jamarco <alexandre.j@gmail.com>
2019-12-25 14:33:35 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,7 +1,7 @@
 # Code your solution here!
 def run_guessing_game
   number = rand(1..6)
-  puts "Try to guess the number"
+  
   guessed_number = gets.chomp.strip
   if guessed_number == "exit"
     puts "Goodbye!"
