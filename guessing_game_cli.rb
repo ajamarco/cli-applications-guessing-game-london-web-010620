ff41e3231af5fc81935be63e716152d3b7b29ed8
Alexandre Jamarco <alexandre.j@gmail.com>
2019-12-25 14:35:07 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -6,7 +6,7 @@ def run_guessing_game
   if guessed_number == "exit"
     puts "Goodbye!"
   elsif guessed_number.to_i == number
-    puts "/You guessed the correct number!/"
+    p "/You guessed the correct number!/"
   else
     puts "/Sorry! The computer guessed #{number}./"
   end
